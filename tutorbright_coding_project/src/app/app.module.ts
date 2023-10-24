fc94a347f7ff5d5955143eb74b7804da980f0eca
marefin <marefin@sfu.ca>
2023-10-24 01:47:30 -0700

    added search feature by name of artist or album

--- tutorbright_coding_project/src/app/app.module.ts
+++ tutorbright_coding_project/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { TopAlbumsComponent } from './top-albums/top-albums.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -15,6 +16,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     HttpClientModule,
     NgbModule
--- tutorbright_coding_project/src/app/top-albums/top-albums.component.ts
+++ tutorbright_coding_project/src/app/top-albums/top-albums.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ItunesService } from '../../services/itunes.service';
 import { ITunesResponse, Album } from '../../models/album.model';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 
 @Component({
@@ -11,10 +11,30 @@ import { Observable } from 'rxjs';
 })
 export class TopAlbumsComponent implements OnInit {
   top100Albums!: Observable<Album[]>;
+  filteredAlbums!: Observable<Album[]>;
+  searchTerm: string = '';
 
   constructor(private itunesService: ItunesService) {}
 
   ngOnInit(): void {
     this.top100Albums = this.itunesService.getTop100Albums();
+    this.filteredAlbums = this.top100Albums; // Initialize with the full list
+  }
+  onSearch() {
+    // Filter the albums based on the search term
+    this.filteredAlbums = this.top100Albums.pipe(
+      map((albums) =>
+        albums.filter((album) =>
+          this.albumMatchesSearchTerm(album, this.searchTerm)
+        )
+      )
+    );
+  }
+  private albumMatchesSearchTerm(album: Album, searchTerm: string): boolean {
+    const artistName = album['im:artist'].label.toLowerCase();
+    const albumName = album['im:name'].label.toLowerCase();
+
+    // Check if either the artist name or album name contains the search term
+    return artistName.includes(searchTerm.toLowerCase()) || albumName.includes(searchTerm.toLowerCase());
   }
 }

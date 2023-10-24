ebed80ff71504867f5a0cad9fc465243f831fb00
marefin <marefin@sfu.ca>
2023-10-24 00:36:52 -0700

    added bootstrap framework

--- tutorbright_coding_project/src/app/app.module.ts
+++ tutorbright_coding_project/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopAlbumsComponent } from './top-albums/top-albums.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
